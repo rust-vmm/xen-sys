e7dc954e1e12b369e8c7fadfdfb53c0e39daf613
Mathieu Poirier <mathieu.poirier@linaro.org>
2021-12-15 10:57:46 -0700

    xen: Move entry_point() to new x86_64 directory
    Move x86_64 entry point to its own directory so that the crate can be
    used by other architecture while keeping as much common code as
    possible.
    
    No change in capability.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- xen/src/x86_64/mod.rs
+++ xen/src/x86_64/mod.rs
@@ -1,6 +1,4 @@
 /*
- * Copyright 2016-2017 Doug Goldstein <cardoe@cardoe.com>
- *
  * Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
  * http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
  * <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
@@ -8,13 +6,4 @@
  * except according to those terms.
  */
 
-#![no_std]
-
-#[cfg(target_arch = "x86_64")]
-mod x86_64;
-#[cfg(target_arch = "x86_64")]
-pub use self::x86_64::*;
-
-// export functionality
-pub mod hypercall;
-
+pub mod entry;
