a9e13d385c1ad6ddaa01487b8fa7b22891ee222e
Mathieu Poirier <mathieu.poirier@linaro.org>
2022-03-04 15:29:24 -0700

    Add foundation for xen-ioctls crate
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

---
+++ crates/xen-ioctls/src/lib.rs
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2021-22 Mathieu Poirier <mathieu.poirier@linaro.org>
+ *
+ * Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+ * http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+ * <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+ * option. This file may not be copied, modified, or distributed
+ * except according to those terms.
+ */
+
+mod sysctl;
+
+#[cfg(target_arch = "x86_64")]
+mod x86_64;
+#[cfg(target_arch = "aarch64")]
+mod aarch64;
