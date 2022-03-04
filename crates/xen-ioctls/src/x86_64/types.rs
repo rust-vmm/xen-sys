e38871bbc312d6aca1affc2e5d9709088a06b002
Mathieu Poirier <mathieu.poirier@linaro.org>
2022-03-04 15:29:24 -0700

    Add foundation for xen-ioctls crate
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

---
+++ crates/xen-ioctls/src/x86_64/types.rs
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2021-22 Mathieu Poirier <mathieu.poirier@linaro.org>
+ *
+ * Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+ * http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+ * <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+ * option. This file may not be copied, modified, or distributed
+ * except according to those terms.
+ */
+
+#[repr(align(8))]
+#[derive(Debug, Copy, Clone)]
+pub struct U64Aligned {
+    pub v: u64,
+} 
