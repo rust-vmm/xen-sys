38610d9efaa3ac9b3272c95dc5d445c22888000e
Mathieu Poirier <mathieu.poirier@linaro.org>
2022-03-23 15:55:33 -0600

    Add BounceBuffer interface
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- crates/xen-ioctls/src/sysctl.rs
+++ crates/xen-ioctls/src/sysctl.rs
@@ -11,7 +11,7 @@
 use std::io::Error;
 use std::fs::{OpenOptions};
 use std::os::unix::io::AsRawFd;
-use libc::{c_void, ioctl, mmap, munmap, MAP_SHARED, PROT_READ, PROT_WRITE};
+use libc::ioctl;
 
 use crate::private::*;
 use crate::sysctl_types::XenSysctl;
@@ -32,44 +32,22 @@ unsafe fn do_ioctl(data: *mut PrivCmdHypercall) -> Result<(), std::io::Error>
     Err(Error::last_os_error())
 }
 
-pub fn do_sysctl(xen_sys_ctl: &mut XenSysctl) ->  Result<(), std::io::Error>
-{
-    let mut ret:Result<(), std::io::Error> = Ok(());
-    let size = round_up(std::mem::size_of::<XenSysctl>() as u64, PAGE_SIZE.into());
-    let fd = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(HYPERCALL_BUFFER_FILE)?;
+pub fn do_sysctl(xen_sys_ctl: &mut XenSysctl) ->  Result<(), std::io::Error> {
+    let bouncebuffer = BounceBuffer::new(std::mem::size_of::<XenSysctl>())?;
+    let vaddr = bouncebuffer.to_vaddr() as *mut XenSysctl;
+    let mut privcmd_hypercall = PrivCmdHypercall {
+        op: __HYPERVISOR_sysctl,
+        arg: [vaddr as u64, 0, 0, 0, 0],
+    };
 
     unsafe {
-        // Setup a bounce buffer for Xen to use.
-        let vaddr: *mut XenSysctl = mmap(0 as *mut c_void, size as usize, PROT_READ | PROT_WRITE, MAP_SHARED, fd.as_raw_fd(), 0) as *mut XenSysctl;
-
-        // Function mmap() returns -1 in case of error.  Casting to i16 or i64
-        // yield the same result.
-        if *(vaddr as *mut i32) == -1 {
-            return Err(Error::last_os_error());
-        }
-
         // Write content of XenSysctl to the bounce buffer so that Xen knows what
         // we are asking for.
         vaddr.write(*xen_sys_ctl);
 
-        let mut privcmd_hypercall = PrivCmdHypercall {
-            op: __HYPERVISOR_sysctl,
-            arg: [vaddr as u64, 0, 0, 0, 0],
-        };
-
-        match do_ioctl(&mut privcmd_hypercall) {
+        do_ioctl(&mut privcmd_hypercall).map(|_| {
             // Read back content from bounce buffer if no errors.
-            Ok(_) => *xen_sys_ctl = vaddr.read(),
-            Err(err) => ret = Err(err),
-        };
-
-        if munmap(vaddr as *mut c_void, size as usize) < 0 {
-            ret = Err(Error::last_os_error());
-        }
+            *xen_sys_ctl = vaddr.read();
+        })
     }
-
-    ret
 }
