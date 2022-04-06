bbf8f005f5dd5bfb6566a57b4230d159f95ecba7
Mathieu Poirier <mathieu.poirier@linaro.org>
2022-04-06 15:20:08 -0600

    Add support for get_domain_info()
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

---
+++ crates/xen-ioctls/src/domctl/xc_types.rs
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2021-22 Mathieu Poirier <mathieu.poirier@linaro.org>
+ *
+ * Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+ * http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+ * <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+ * option. This file may not be copied, modified, or distributed
+ * except according to those terms.
+ */
+
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+
+use std::convert::TryFrom;
+
+#[cfg(target_arch = "x86_64")]
+use crate::x86_64::types::*;
+#[cfg(target_arch = "aarch64")]
+use crate::aarch64::types::*;
+
+use crate::domctl::types::*;
+use crate::private::PAGE_SHIFT;
+
+#[derive(Debug, Default, Copy, Clone)]
+// tools/include/xenctrl.h::xc_dominfo 
+pub struct XcDominfo {
+    pub domid: u16,
+    pub ssidref: u32,
+    pub dying: bool,
+    pub crashed: bool,
+    pub shutdown: bool,
+    pub paused: bool,
+    pub blocked: bool,
+    pub running: bool,
+    pub hvm: bool,
+    pub debugged: bool,
+    pub xenstore: bool,
+    pub hap: bool,
+    pub shutdown_reason: u32, /* only meaningful if shutdown==1 */
+    pub nr_pages: u64, /* current number, not maximum */
+    pub nr_outstanding_pages: u64,
+    pub nr_shared_pages: u64,
+    pub nr_paged_pages: u64,
+    pub shared_info_frame: u64,
+    pub cpu_time: u64,
+    pub max_memkb: u64,
+    pub nr_online_vcpus: u32,
+    pub max_vcpu_id: u32,
+    pub handle: [u8; 16],
+    pub cpupool: u32,
+    pub gpaddr_bits: u8,
+    pub arch_config: XenArchDomainconfig,
+}
+
+impl TryFrom<XenDomctlGetDomainInfo> for XcDominfo {
+    type Error = ();
+
+    fn try_from(info: XenDomctlGetDomainInfo) -> Result<Self, Self::Error> {
+        let shutdown_reason: u32 = (info.flags >> XEN_DOMINF_shutdownshift) &
+                                    XEN_DOMINF_shutdownmask;
+
+        let mut shutdown: bool = (info.flags & XEN_DOMINF_shutdown) != 0;
+        let mut crashed: bool = false;
+        if shutdown && shutdown_reason == SHUTDOWN_crash {
+            shutdown = false;
+            crashed = true;
+        }
+
+        Ok(XcDominfo {
+            domid: info.domain,
+            dying: (info.flags & XEN_DOMINF_dying) != 0,
+            crashed,
+            shutdown,
+            paused: (info.flags & XEN_DOMINF_paused) != 0,
+            blocked: (info.flags & XEN_DOMINF_blocked) != 0,
+            running: (info.flags & XEN_DOMINF_running) != 0,
+            hvm: (info.flags & XEN_DOMINF_hvm_guest) != 0,
+            debugged: (info.flags & XEN_DOMINF_debugged) != 0,
+            xenstore: (info.flags & XEN_DOMINF_xs_domain) != 0,
+            hap: (info.flags & XEN_DOMINF_hap) != 0,
+            shutdown_reason,
+            ssidref: info.ssidref,
+            nr_pages: info.tot_pages.v,
+            nr_outstanding_pages: info.outstanding_pages.v,
+            nr_shared_pages: info.shr_pages.v,
+            nr_paged_pages: info.paged_pages.v,
+            max_memkb: info.max_pages.v << (PAGE_SHIFT - 10),
+            shared_info_frame: info.shared_info_frame.v,
+            cpu_time: info.cpu_time.v,
+            nr_online_vcpus: info.nr_online_vcpus,
+            max_vcpu_id: info.max_vcpu_id,
+            handle: info.handle,
+            cpupool: info.cpupool,
+            gpaddr_bits: info.gpaddr_bits,
+            arch_config: info.arch_config,
+        })
+    }
+ }
+
+
--- examples/ioctls/src/main.rs
+++ examples/ioctls/src/main.rs
@@ -8,28 +8,31 @@
  * except according to those terms.
  */
 
-use xen_ioctls::sysctl_types::*;
-use xen_ioctls::xen_types::*;
-use xen_ioctls::private::PrivCmdHypercall;
-use xen_ioctls::xc_misc::*;
+use xen_ioctls::sysctl::types::*;
+use xen_ioctls::domctl::types::*;
+use xen_ioctls::domctl::domctl::*;
+use xen_ioctls::sysctl::sysctl::*;
 
 fn main() {
 
     println!("size of XenSysctlPhysinfo: {}", std::mem::size_of::<XenSysctlPhysinfo>());
     println!("size of XenSysctlGetDomaininfolist: {}", std::mem::size_of::<XenSysctlGetdomaininfolist>());
-    println!("size of PrivCmdHypercall: {}", std::mem::size_of::<PrivCmdHypercall>());
     println!("size of XenSysctl: {}", std::mem::size_of::<XenSysctl>());
+    println!("size of XenDomctl: {}", std::mem::size_of::<XenDomctl>());
     println!("size of XenDomctlGetDomainInfo: {}", std::mem::size_of::<XenDomctlGetDomainInfo>());
 
-    match get_physinfo() {
+    match xc_physinfo() {
         Ok(physinfo) => println!("physinfo: {:?}", physinfo),
         Err(err) => println!("err: {}", err),
     }
 
-    let domain_list = get_domain_infolist(0, 1024).unwrap();
+    let dominfo_list = xc_domain_info(0, 10);
+    for dominfo in dominfo_list {
+        println!("domaininfo: {:?}", dominfo);
+    }
+
+    let domain_list = xc_domain_getinfolist(0, 1024).unwrap();
     for domain in domain_list {
-        println!("domain: {} flags: {:x}", domain.domain, domain.flags);
-        println!("tot_pages: {:x} max_pages: {:x}", domain.tot_pages, domain.max_pages);
-        println!("nr_online_vcpus: {} max_vcpuid: {}", domain.nr_online_vcpus, domain.max_vcpu_id);
+        println!("domainlist: {:?}", domain);
     }
 }
