0518ee6f7d882ca00674a2caa96602c02f310900
Mathieu Poirier <mathieu.poirier@linaro.org>
2022-04-27 16:43:12 -0600

    Remove test code no longer needed
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- examples/ioctls/src/main.rs
+++ examples/ioctls/src/main.rs
@@ -8,18 +8,10 @@
  * except according to those terms.
  */
 
-use xen_ioctls::sysctl::types::*;
-use xen_ioctls::domctl::types::*;
 use xen_ioctls::domctl::domctl::*;
 use xen_ioctls::sysctl::sysctl::*;
 
 fn main() {
-
-    println!("size of XenSysctlPhysinfo: {}", std::mem::size_of::<XenSysctlPhysinfo>());
-    println!("size of XenSysctlGetDomaininfolist: {}", std::mem::size_of::<XenSysctlGetdomaininfolist>());
-    println!("size of XenSysctl: {}", std::mem::size_of::<XenSysctl>());
-    println!("size of XenDomctl: {}", std::mem::size_of::<XenDomctl>());
-    println!("size of XenDomctlGetDomainInfo: {}", std::mem::size_of::<XenDomctlGetDomainInfo>());
 
     match xc_physinfo() {
         Ok(physinfo) => println!("physinfo: {:?}", physinfo),
