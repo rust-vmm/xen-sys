8228d131971b4210a9957494e42c0ee34ac2ca91
Mathieu Poirier <mathieu.poirier@linaro.org>
2022-05-02 15:27:44 -0600

    Add initial support for xenforeignmemory_map()
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- crates/xen-ioctls/src/xfm/xfm.rs
+++ crates/xen-ioctls/src/xfm/xfm.rs
@@ -8,10 +8,13 @@
  * except according to those terms.
  */
 
+use std::convert::TryInto;
 use std::fs::OpenOptions;
 use std::io::{Error, ErrorKind};
 use std::os::unix::io::AsRawFd;
-use libc::{c_void, mmap, munmap, MAP_SHARED, MAP_PRIVATE, PROT_READ, PROT_WRITE};
+use std::{thread, time};
+use std::ptr;
+use libc::{c_int, c_void, mmap, munmap, MAP_SHARED, MAP_PRIVATE, PROT_READ, PROT_WRITE, ENOENT};
 
 use crate::private::*;
 use crate::xfm::types::*;
@@ -98,6 +101,127 @@ pub fn xenforeignmemory_map_resource(domid: u16, r#type: u32, id: u32, frame: u3
 pub fn xenforeignmemory_unmap_resource(resource: &XenForeignMemoryResourceHandle)-> Result<(), std::io::Error> {
     unsafe {
         if munmap(resource.addr, (resource.nr_frames << PAGE_SHIFT) as usize) < 0 {
+            Err(Error::last_os_error())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+fn retry_paged_pages(domid: u16, addr: *mut c_void, pages: u64,
+                     arr: *const u64, err: *mut c_int)
+                     -> Result<(), std::io::Error> {
+    let mut i = 0;
+    let mut batch_start = 0;
+
+    unsafe {
+        while i < pages {
+            /* Look for the first page that faulted */
+            while i < pages {
+                if *err.add(i as usize) == -ENOENT {
+                    batch_start = i;
+                    break;
+                }
+
+                i += 1;
+            }
+
+            let mut privcmd_mmapbatch_v2 = PrivCmdMmapBatchV2 {
+                num: 1,
+                dom: domid,
+                addr: (addr as *mut u8).add((i << PAGE_SHIFT) as usize) as *mut c_void,
+                arr: arr.add(i as usize),
+                err: err.add(i as usize),
+            };
+
+            i += 1;
+
+            /* Try to lump as many_ consecutive_ faulted  pages in the same request */
+            while i < pages  {
+                if *err.add(i as usize) != -ENOENT {
+                    break;
+                }
+
+                i += 1;
+                privcmd_mmapbatch_v2.num += 1;
+            }
+
+            /*
+             * The expression "&mut privcmd_mmapbatch_v2 as *mut _" creates a reference
+             * to privcmd_mmapbatch_v2 before casting it to a *mut c_void
+             */
+            let privcmd_ptr: *mut c_void = &mut privcmd_mmapbatch_v2 as *mut _ as *mut c_void;
+
+            match do_ioctl(IOCTL_PRIVCMD_MMAPBATCH_V2, privcmd_ptr) {
+                Ok(_) => {
+                    continue;
+                }
+                Err(e) => {
+                    if e.raw_os_error() == Some(libc::ENOENT) {
+                        /* Something went wrong with this batch, retry it */
+                        i = batch_start;
+                        thread::sleep(time::Duration::from_micros(100));
+                        continue;
+                    }
+
+                    return Err(e);
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+pub fn xenforeignmemory_map(domid: u16, prot: i32, pages: u64,
+                            arr: *const u64, err: *mut c_int)
+                            -> Result<*mut c_void, std::io::Error> {
+    let mut err_vec: Vec<c_int> = vec![0; pages as usize];
+    let mut err_array: *mut c_int = err;
+    let num: u32 = pages.try_into().map_err(|_| {
+        return ErrorKind::InvalidInput;
+    })?;
+
+    let addr: *mut c_void = map_from_address(ptr::null_mut(),
+                                            (pages << PAGE_SHIFT) as usize,
+                                            prot, MAP_SHARED, 0)?;
+
+    if err.is_null() {
+        err_array = err_vec.as_mut_ptr();
+    }
+
+    let mut privcmd_mmapbatch_v2 = PrivCmdMmapBatchV2 {
+        num: num,
+        dom: domid,
+        addr: addr,
+        arr: arr,
+        err: err_array,
+    };
+    /*
+     * The expression "&mut privcmd_mmapbatch_v2 as *mut _" creates a reference
+     * to privcmd_mmapbatch_v2 before casting it to a *mut c_void
+     */
+    let privcmd_ptr: *mut c_void = &mut privcmd_mmapbatch_v2 as *mut _ as *mut c_void;
+
+    unsafe {
+        match do_ioctl(IOCTL_PRIVCMD_MMAPBATCH_V2, privcmd_ptr) {
+            Ok(_) => {
+                Ok(addr)
+            }
+            Err(e) => {
+                if e.raw_os_error() != Some(libc::ENOENT) {
+                    return Err(e);
+                }
+
+                retry_paged_pages(domid, addr, pages, arr, err_array).map(|_| { addr })
+            }
+        }
+    }
+}
+
+pub fn xenforeignmemory_unmap(addr: *mut c_void, pages: u64)-> Result<(), std::io::Error> {
+    unsafe {
+        if munmap(addr, (pages << PAGE_SHIFT) as usize) < 0 {
             Err(Error::last_os_error())
         } else {
             Ok(())
